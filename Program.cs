b22c284e6da848943fbdb15c4e07ac8322688447
= <=>
2023-04-01 17:49:18 -0300

    Troco de mercado

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿void Shin (int fun)
+{
+     if (fun == 00)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    }
+    else if (fun == 01)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+    }
+        else if (fun == 02)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+    }
+}
+void barra()
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("<=============================>");
+}
+Console.Clear();
+
+
+Console.WriteLine("Qual o valor que você tem no cartão?");
+decimal valor_Usuario= Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Qual o valor do produto?");
+decimal valor_Preco= Convert.ToDecimal(Console.ReadLine());
+decimal Troco= valor_Usuario-valor_Preco;
+
+
+barra();
+Shin(0);
+Console.WriteLine("|======   Nota  Fiscal  ======|");
+barra();
+Console.WriteLine($"| Seu cartão de débito: {Convert.ToString(valor_Usuario).PadRight(18,'.')} |"); // 18 pontos.
+barra();
+Shin(0);
+Console.WriteLine($"| Preço do produto: {Convert.ToString(valor_Preco).PadRight(18,'.')} |"); // 18 pontos.
+barra();
+Shin(0);
+Console.WriteLine($"| Seu troco: {Convert.ToString(Troco).PadRight(18,'.')} |"); // 18 pontos.
+barra(); 
+Console.ResetColor();
